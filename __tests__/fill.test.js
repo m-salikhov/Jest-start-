52735df2b8c61349e7f4338b386f40df2a629588
m-salikhov <andvary@inbox.ru>
2024-01-04 21:08:24 +0300

    less 10 fill 2

--- __tests__/fill.test.js
+++ __tests__/fill.test.js
@@ -4,6 +4,7 @@ const array = [1, 2, 3, 4];
 
 test("test fill function", () => {
   expect(fill([...array], "*", 1, 3)).toEqual([1, "*", "*", 4]);
+  expect(fill([...array], "*", 3, 1)).toEqual([1, 2, 3, 4]);
   expect(fill([...array], "*")).toEqual(["*", "*", "*", "*"]);
   expect(fill([...array], "*", -10)).toEqual(["*", "*", "*", "*"]);
   expect(fill([...array], "*", 4)).toEqual([1, 2, 3, 4]);
